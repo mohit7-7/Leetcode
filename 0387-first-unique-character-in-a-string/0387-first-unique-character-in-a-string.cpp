f763df2ce117f7f68e83b5d5ffcdeaa56af571a2
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2024-07-22 17:10:13 +0530

    Time: 46 ms (31.49%), Space: 25.8 MB (5.50%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,13 +1,44 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//        vector<int> arr(26,0);
+//         for( char c:s){
+//             arr[c-'a']++;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             if(arr[s[i]-'a']==1) return i;
+//         }
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
-    int firstUniqChar(string s) {
-       vector<int> arr(26,0);
-        for( char c:s){
-            arr[c-97]++;
+    int firstUniqChar(std::string s) {
+        vector<int> count(26, 0);
+        queue<pair<char, int>> q;
+
+        // Step 1: Count the frequency of each character and maintain the queue
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s[i];
+            count[c - 'a']++;
+            q.push({c, i});
         }
-        for(int i=0;i<s.length();i++){
-            if(arr[s[i]-'a']==1) return i;
+
+        // Step 2: Find the first unique character
+        while (!q.empty()) {
+            auto front = q.front();
+            char c = front.first;
+            int index = front.second;
+
+            if (count[c - 'a'] == 1) {
+                return index;
+            } else {
+                q.pop();
+            }
         }
+
         return -1;
     }
 };
