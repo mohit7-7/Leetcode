b0470e0a58e0f53c09c55c4a13e143c1981deb05
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2024-11-12 14:51:59 +0530

    Time: 55 ms (68.85%), Space: 91.5 MB (88.51%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-
-    int customBinarySearch(vector<vector<int>>& items, int queryPrice) {
-        int l = 0;
-        int r = items.size()-1;
-
-        int mid;
-        int maxBeauty = 0;
-
-        while(l <= r) {
-            mid = l + (r-l)/2;
-
-            if(items[mid][0] > queryPrice) {
-                r = mid-1; //ignore current and right side items. Move to left now.
-            } else {
-                maxBeauty = max(maxBeauty, items[mid][1]);
-                l = mid+1;
+    
+    int custombs(vector<vector<int>>&items, int querysize){
+        int l=0,r=items.size()-1,mid;
+        int maxi= 0;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(items[mid][0]>querysize){
+                r=mid-1;
+            }
+            else{
+                maxi = max(items[mid][1],maxi);
+                l=mid+1;
             }
         }
-
-        return maxBeauty;
+        return maxi;
     }
-
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        int n = items.size();
-        int m = queries.size();
-
-        vector<int> result(m);
-
-        //step-1 : Sort the array based on price in asencind order
-        sort(begin(items), end(items)); //{pricei, beautyi}
-
-        int maxBeautySeen = items[0][1];
-        for(int i = 1; i < n; i++) {
-            maxBeautySeen = max(maxBeautySeen, items[i][1]);
-            items[i][1] = maxBeautySeen;
+        int n=items.size();
+        int m= queries.size();
+        vector<int>ans(m);
+        sort(items.begin(), items.end());
+        int maxeleseen=items[0][1];
+        for(int i=1;i<n;i++){
+            maxeleseen = max(maxeleseen,items[i][1]);
+            items[i][1]= maxeleseen;
         }
-
-
-        for(int i = 0; i < m; i++) {
-            int queryPrice = queries[i];
-            result[i] = customBinarySearch(items, queryPrice);
+        
+        for(int i=0;i<m;i++){
+            int querysize= queries[i];
+            ans[i]=custombs(items,querysize);
         }
-
-        return result;
-
+        return ans;
     }
 };
