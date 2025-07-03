f17b2e966ef3313d1e113d61f6afffb26e546c2f
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-07-03 18:48:11 +0530

    Time: 2 ms (64.66%), Space: 10.6 MB (29.74%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-
     void makenew(string &s){
         string temp="";
         for(int i=0;i<s.size();i++){
-            temp+= (s[i]+1%26);
+            temp+= (s[i]+1);
         }
         s+= temp;
     }
+
     char kthCharacter(int k) {
         string org= "a";
         while(org.size()<k){
