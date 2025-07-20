72830d03e37faf69b5937975e9d35695a46022c0
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-07-20 11:29:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
         int n= nums.size()/3;
-        int total= 3*n;
-
-        priority_queue<int> maxheap;
-        vector<long long>leftmin(total,0);
-        long long sum=0;
+        vector<int>isselected(3*n,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> minheap;
         for(int i=0;i<2*n;i++){
-            maxheap.push(nums[i]);
-            sum+= nums[i];
-            while(maxheap.size()>n){
-                sum -= maxheap.top();
-                maxheap.pop();
+            minheap.push({nums[i],i});
+        }
+        long long sumfirst=0;
+        for(int i=0;i<n;i++){
+            int indx=minheap.top().second;
+            int val= minheap.top().first;
+            sumfirst+= val;
+            isselected[indx]=1;
+            minheap.pop();
+        }
+        priority_queue<int>maxheap;
+        for(int i=n;i<3*n;i++){
+            if(!isselected[i]){
+                maxheap.push(nums[i]);
             }
-            leftmin[i]= sum;
         }
 
-        priority_queue<int,vector<int>, greater<int>>minheap;
-        vector<long long>rightmax(total,0);
-        sum=0;
-        for(int i=total-1;i>n-1;i--){
-            minheap.push(nums[i]);
-            sum+= nums[i];
-            while(minheap.size()>n){
-                sum-= minheap.top();
-                minheap.pop();
-            }
-            rightmax[i]= sum;
+        long long sumsecond=0;
+        for(int i=0;i<n;i++){
+            sumsecond+= maxheap.top();
+            maxheap.pop();
         }
 
-        long long ans= LLONG_MAX;
-        for(int i=n-1;i<2*n;i++){
-            ans= min(ans, leftmin[i]-rightmax[i+1]);
-        }
-        return ans;
+        return sumfirst-sumsecond;
     }
 };
