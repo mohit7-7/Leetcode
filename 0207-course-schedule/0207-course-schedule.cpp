e9e3018366cac47c2753d2784b80d8f92668fbda
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-07-25 14:34:02 +0530

    Time: 5 ms (59.12%), Space: 23.2 MB (5.17%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfscycle(int node, vector<bool>&visited, vector<vector<int>>&adjlist, vector<bool>inrec){
+        visited[node]= true;
+        inrec[node]= true;
+        for(auto &neighbour : adjlist[node]){
+            if(visited[neighbour] && inrec[neighbour] )
+                return true;
+            if(!visited[neighbour]){
+                if(dfscycle(neighbour, visited,adjlist, inrec))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adjlist(n);
+        for(auto &edge: edges){
+            int u=edge[0], v= edge[1];
+            adjlist[u].push_back(v);
+        }
+
+        vector<bool>visited(n,false);
+        vector<bool>inrec(n,false);
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(dfscycle(i, visited, adjlist, inrec)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+};
