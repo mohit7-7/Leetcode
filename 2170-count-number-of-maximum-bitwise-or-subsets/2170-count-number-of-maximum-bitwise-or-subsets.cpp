836f9ac89b91e2934e3104d5f821922f8b58474a
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-07-28 17:19:05 +0530

    Time: 8 ms (80.56%), Space: 26 MB (20.41%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
         vector<int> subsetsor={0};
+        int max= 0, count=0;
         for (int num : nums) {
             int size = subsetsor.size();
             for (int i = 0; i < size; ++i) {
-                int newSubsetor = subsetsor[i];
-                newSubsetor |= num;
+                int newSubsetor = subsetsor[i]| num;
+                if(newSubsetor> max){
+                    max= newSubsetor;
+                    count=1;
+                }
+                else if(max==newSubsetor ) count++;
                 subsetsor.push_back(newSubsetor);
             }
         }
-        int maximum= *max_element(subsetsor.begin(), subsetsor.end());
-        return count(subsetsor.begin(), subsetsor.end(), maximum);
+        // int maximum= *max_element(subsetsor.begin(), subsetsor.end());
+        // return count(subsetsor.begin(), subsetsor.end(), maximum);
+        return count;
     }
 };
