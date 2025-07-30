616f716dab7a080a0e7ed61308c4c977cd26efe2
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-07-31 03:04:37 +0530

    Time: 0 ms (100%), Space: 23.8 MB (53.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,10 +4,10 @@ public:
         int n= nums.size();
         int i=0,j=0;
         while(j<n){
-            if(nums[j]!=0){
-                swap(nums[i], nums[j]);
-                i++;
+            if(nums[j]!=0 && nums[i]==0){
+                swap(nums[j],nums[i]);
             }
+            if(nums[i]!=0)i++;
             j++;
         }
     }
