947b9259314a43735fc9cf6afaad9be6446fac32
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-08-07 11:49:48 +0530

    Time: 5 ms (84.93%), Space: 10.2 MB (93.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,22 @@
+#include <vector>
+#include <cstdint>
+using namespace std;
+
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n= coins.size();
-        int dp[n+1][amount+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=amount;j++){
-                if(i==0) dp[i][j]=0;
-                if(j==0) dp[i][j]=1;
+        vector<int64_t> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; ++j) {
+                dp[j] += dp[j - coin];
+
+                // Optional: clamp to avoid overflow
+                if (dp[j] > INT32_MAX) dp[j] = INT32_MAX;
             }
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){
-                    dp[i][j]= dp[i][j-coins[i-1]]+ dp[i-1][j];
-                }
-                else{
-                    dp[i][j]= dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][amount];
+
+        return static_cast<int>(dp[amount]);
     }
 };
