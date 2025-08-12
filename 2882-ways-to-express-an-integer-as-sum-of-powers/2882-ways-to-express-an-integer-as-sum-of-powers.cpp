88f9103830fcdccbf91ccca891d90da8e18e24ff
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-08-12 15:54:22 +0530

    Time: 103 ms (68.63%), Space: 73.4 MB (51.85%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mod= 1e9+7;
+    int numberOfWays(int target, int x) {
+        vector<int>poss;
+        for(int i=1;target>=pow(i,x);i++){
+            poss.push_back(pow(i,x));
+        }
+        int n= poss.size();
+        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                int take=0;
+                if(poss[i-1]<= j){
+                    take= dp[i-1][j-poss[i-1]];
+                }
+                int nottake = dp[i-1][j];
+                dp[i][j]= (take+nottake)%mod;
+            }
+        }
+        return dp[n][target]%mod;
+    }
+};
