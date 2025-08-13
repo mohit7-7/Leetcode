b746e7bef6962aaa3099802107e06e58cf2c478f
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-08-13 22:50:02 +0530

    Time: 4 ms (1.01%), Space: 8 MB (18.1%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<1) return false;
-        return  (n&(n-1))==0;
+        int low=0, high = log2(n);
+        while(low<=high){
+            int mid= (low+high)/2;
+            int val= (1<<mid);
+            if(val==n) return true;
+            if(val < n) low= mid+1;
+            else high= mid-1;
+        }
+        return false;
     }
 };
