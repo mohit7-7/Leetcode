b8726d2f385b5ae7d555a9419fda8abcbf755baf
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-08-14 06:30:23 +0530

    Time: 0 ms (100%), Space: 9.7 MB (10.87%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     unordered_set<string>st;
     bool reorderedPowerOf2(int n) {
-        for(int p=0;p<=29;p++){
+        for(int p=0;p<=30;p++){
             string power= to_string(1<<p);
             sort(begin(power), end(power));
             st.insert(power);
@@ -10,6 +10,7 @@ public:
         }
 
         string input= to_string(n);
+        sort(begin(input), end(input));
         return st.count(input);
     }
 };
