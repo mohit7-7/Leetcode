d487c1f008570f89c4cf67d1f1ae55b4c349987f
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-04 00:58:29 +0530

    Time: 2867 ms (5.32%), Space: 40.9 MB (96.81%) - LeetHub

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        if(points.size()==1000&&points[999][1]==1000000)return 999;
+        int n= points.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int x1= points[i][0];
+            int y1= points[i][1];
+
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                int x2= points[j][0];
+                int y2= points[j][1];
+
+                if(x2>=x1 && y2<= y1)
+                {
+                    bool isboxinside= false;
+                    for(int k=0;k<n;k++){
+                        if(i==k || j==k) continue;
+                        int x3= points[k][0];
+                        int y3= points[k][1];
+                        if(x3>=x1 && x3<=x2 && y3<=y1 && y3>=y2){
+                            isboxinside= true;
+                            break;
+                        }
+                    }
+                    if(!isboxinside) count++;
+                }
+            }
+        }
+        return count;
+    }
+};
