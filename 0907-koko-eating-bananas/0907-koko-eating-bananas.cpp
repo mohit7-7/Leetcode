66e882338ec4c3a0d84f3eb2c98c2e9526686ee5
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-06 18:13:50 +0530

    Time: 18 ms (12.66%), Space: 23.1 MB (11.63%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -3,8 +3,7 @@ public:
     bool ispossible(int mid, vector<int>&piles, int h){
         long long time=0;
         for(int num: piles){
-            time += (num + mid - 1) / mid;  // integer ceiling
-            // if(time>h) return false;
+            time += ceil((double)num/(double)mid);
         }
         return time<=h;
     }
@@ -12,7 +11,7 @@ public:
         int low= 1, high= *max_element(piles.begin(), piles.end());
         int ans=-1;
         while(low<=high){
-            int mid= low+(high-low)/2;
+            int mid= (low+high)/2;
             if(ispossible(mid,piles,h)){
                 ans= mid;
                 high = mid-1;
