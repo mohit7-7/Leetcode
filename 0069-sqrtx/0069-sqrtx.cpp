7dde8dfb4a8486f5ee741a830d7db56244773f4b
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-06 14:13:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,20 @@
+#define ll long long 
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int low = 1, high= x;
+        int ans=0;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(((ll)mid*(ll)mid)==x) return mid;
+            if((mid * mid)<x){
+                ans= mid;
+                low= mid+1;
+            }
+            else{
+                high= mid-1;
+            }
+        }
+        return ans;
     }
-}; 
+};
