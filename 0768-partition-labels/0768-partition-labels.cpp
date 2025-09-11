7e7fcffe9ce23f12e3ff0e9eac5845784ce83cb5
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-12 01:49:45 +0530

    Time: 0 ms (100%), Space: 9 MB (24.27%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,int>mp;
+
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]= i;
+        }
+        vector<int>ans;
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.size();i++){
+            end= max(end, mp[s[i]]);
+            if(i== end){
+                ans.push_back(end-start+1);
+                start= end+1;
+            }
+        }
+        return ans;
+    }
+};
