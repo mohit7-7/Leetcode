e91bc08da85ec80e86f36e3c0c34504b09f89a61
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-15 03:32:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,13 @@
 class Solution {
-public:
-    // int dpsol(int n, vector<int>&dp){ 
-    //     if(n==0 || n==1) return 1;
-    //     if(dp[n]!=-1)return dp[n];
-    //     dp[n]= dpsol(n-1,dp)+ dpsol(n-2,dp);
-    //     return dp[n];
-    // }
-    int climbStairs(int n) {
-        // vector<int> dp(n+1,-1);
-        // return dpsol(n, dp);
-        vector<int>dp(n+1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+public: 
+    int f(int n, vector<int>&dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]= f(n-1,dp)+f(n-2,dp);
         return dp[n];
-    } 
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
 };
