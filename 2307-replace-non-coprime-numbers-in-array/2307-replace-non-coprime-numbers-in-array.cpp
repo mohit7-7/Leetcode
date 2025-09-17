09e55d0cb88e2914f67b23c7936605c9858574f1
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-17 23:19:48 +0530

    Time: 26 ms (41.01%), Space: 125.3 MB (44.7%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -3,17 +3,18 @@ public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
         vector<int>ans;
         for(int num: nums){
-            ans.push_back(num);
-            auto gcdd=  [](int num1,int num2){
-                return (num2==0)? num1: gcd(num2, num1%num2);
+            auto gcdd = [&](auto self, int a, int b) -> int {
+                return (b == 0) ? a : self(self, b, a % b);
             };
+
+            ans.push_back(num);
             while(ans.size()>1){
                 int num1= ans.back();
                 int num2= ans[ans.size()-2];
-                if(gcdd(num1, num2)==1) break;
+                if(gcdd(gcdd,num1, num2)==1) break;
                 ans.pop_back();
                 ans.pop_back();
-                int lcmm= (1LL*num1*num2)/gcd(num1, num2);
+                int lcmm= (1LL*num1*num2)/gcdd(gcdd,num1, num2);
                 ans.push_back(lcmm);
             }
         }
