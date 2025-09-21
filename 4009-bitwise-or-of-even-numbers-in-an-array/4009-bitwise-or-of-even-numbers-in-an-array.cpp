7336d37e6e62bdec6934d0aa15548438fd0d76af
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-21 15:40:23 +0530

    Time: 0 ms (100%), Space: 24.5 MB (100%) - LeetHub

---
+++ 4009-bitwise-or-of-even-numbers-in-an-array/4009-bitwise-or-of-even-numbers-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int ans=0;
+        for(int num: nums){
+            if(num%2==0){
+                ans |= num;
+            }
+        }
+        return ans;
+    }
+};
