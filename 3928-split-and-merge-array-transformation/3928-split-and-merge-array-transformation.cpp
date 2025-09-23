d13d0a336ecf2562217c924c6a9e13cdd0de9ced
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-24 04:04:50 +0530

    Time: 211 ms (90.39%), Space: 81.9 MB (87.21%) - LeetHub

--- 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.cpp
+++ 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     int minSplitMerge(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1==nums2)return 0;
-        int n=nums1.size();
-
-        queue<pair<vector<int>,int>>q;
-        set<vector<int>>vis;
-        q.push({nums1,0});
+        if(nums1 == nums2) return 0;
+        int n = nums1.size();
+        queue<vector<int>> q;
+        q.push(nums1);
+        map<vector<int>, int> dist;
+        dist[nums1] = 0;
         while(!q.empty()){
-            auto [curr,steps]=q.front();
+            auto it = q.front();
             q.pop();
-            if(curr==nums2)return steps;
-            for(int i=0;i<n;i++){
-                for(int j=i;j<n;j++){
-                    vector<int>currSub(curr.begin()+i,curr.begin()+j+1);
-                    vector<int>restArray;
-                    for(int k=0;k<n;k++){
-                        if(k<i || k>j)restArray.push_back(curr[k]);
+            int d = dist[it];
+            
+            for(int l = 0; l < n; l++){
+                
+                for(int r = l; r < n; r++){
+                    vector<int> v;
+            vector<int> temp;
+                    for(int k = l; k <= r;k++)v.push_back(it[k]);
+                    for(int i = 0; i < l; i++) temp.push_back(it[i]);
+                    for(int i = r + 1; i < n; i++) temp.push_back(it[i]);
+
+                    for(int i = 0; i < (int)temp.size(); i++){
+                        vector<int> temp2 = temp;
+                        temp2.insert(temp2.begin() + i, v.begin(), v.end());
+                        if(dist.find(temp2) == dist.end()){
+                        dist[temp2] = 1 + d;
+                        if(temp2 == nums2) return dist[temp2];
+                        q.push(temp2);
+                        
+                    }
                     }
 
-                    for(int k=0;k<=restArray.size();k++){
-                        vector<int>temp;
-                        temp.insert(temp.end(),restArray.begin(),restArray.begin()+k);
-                        temp.insert(temp.end(),currSub.begin(),currSub.end());
-                        temp.insert(temp.end(),restArray.begin()+k,restArray.end());
-
-                        if(vis.find(temp)==vis.end()){
-                            vis.insert(temp);
-                            q.push({temp,steps+1});
-                        }
-                    }
                     
                 }
             }
         }
-        return n-1;
-        
+        return -1;
         
     }
 };
