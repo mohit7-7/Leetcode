dd6fe41d1d0d6589773ab9b601567ec19a355e30
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-23 18:57:35 +0530

    Time: 776 ms (74.77%), Space: 377.7 MB (71.73%) - LeetHub

--- 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.cpp
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    int findksmallest(vector<string>& nums, int k){
-    vector<pair<string,int>> arr;
-    for(int i=0;i<nums.size();i++){
-        arr.push_back({nums[i],i});
-    }
-    nth_element(arr.begin(), arr.begin()+k-1, arr.end());
-    // kth smallest will be at arr[k-1] but need full ordering for tie-breaking
-    sort(arr.begin(), arr.begin()+k);
-    return arr[k-1].second;
-}
-
-
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector<int>ans;
-        int numsize= nums[0].length();
-        for(auto& query: queries){
-            int k= query[0];
-            int trim= query[1];
-            int trimindx = numsize-trim;
-            vector<string> newnums;
-            
-            for(string num : nums){
-                newnums.push_back((num.substr(trimindx)));
+        vector<int> res;
+        int n = nums.size();
+        for(auto &q:queries) {
+            vector<pair<string,int>> pq;
+            for(int i=0;i<n; i++) {
+                pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
             }
-            
-            int val= findksmallest(newnums,k);
-            ans.push_back(val);
-
+            sort(pq.begin(), pq.end());
+            res.push_back(pq[q[0]-1].second);
         }
-        return ans;
+        return res;
     }
 };
