001dd1199e9bc3bff7578ba91f469e61356b915b
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-09-28 01:08:45 +0530

    Time: 8 ms (37.48%), Space: 115 MB (12.62%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int n= nums.size();
-        deque<int>flipq;
+        int n = nums.size();
+        int flips=0;
         int flipcount=0;
-        int totalflips=0;
+        vector<int>isflipped(n,0);
         for(int i=0;i<n;i++){
-            if(i>=k){
-                flipcount -= flipq.front();
-                flipq.pop_front();
+            if(i>=k && isflipped[i-k]==true){
+                flipcount--;
             }
-            if(nums[i] == flipcount%2){
-                if(i+k>n) return -1;
-                flipq.push_back(1);
+            if(nums[i]== flipcount%2){
+                if(i+k >n ) return -1;
                 flipcount++;
-                totalflips++;
-            }
-            else{
-                flipq.push_back(0);
+                flips++;
+                isflipped[i]= true;
             }
         }
-        return totalflips;
+        return flips;
     }
 };
