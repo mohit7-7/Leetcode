01c079236a1c0b61690a6ec66ff8dd5a3e1f693c
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-06 00:49:34 +0530

    Time: 0 ms (100%), Space: 171.3 MB (0%) - LeetHub

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/4033-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/4033-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -3,15 +3,14 @@ public:
     int longestSubsequence(vector<int>& nums) {
         int n= nums.size();
         int xorr=0;
+        bool flag= false;
         for(int num:nums){
+            if(num!=0) flag = true;
             xorr ^= num;
         }
         if(xorr) return n;
-        bool flag = false;
-        for(int num: nums){
-            if(num!=0) flag = true;
-        }
         if(!flag) return 0;
         else return n-1;
     }
 };
+
