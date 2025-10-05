b832a2899bb52b873b9538ce6bc4682530f7b500
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-06 00:20:05 +0530

    Time: 0 ms (100%), Space: 32.5 MB (100%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int ans=0;
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            ans += i%2 ? -nums[i]: nums[i]; 
+        }
+        return ans;
+    }
+};
