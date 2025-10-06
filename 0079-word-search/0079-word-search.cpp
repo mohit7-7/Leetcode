db8c5d0731cc8987c707cda4a7e3780038938426
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-07 01:28:15 +0530

    Time: 259 ms (58.72%), Space: 10.8 MB (54.05%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> directions= { {0,1}, {0,-1}, {1,0}, {-1,0} };
-    int m,n;
-    bool solve(int i, int j, vector<vector<char>> & board, int indx, string word){
-        if(indx == word.length())  return true;
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='$' ) return false;
-        if( board[i][j]!= word[indx]) return false;  
-        char temp= board[i][j];
-        board[i][j]='$';
+    int dx[4]= {-1, 0, 1, 0};
+    int dy[4]= {0, 1, 0,-1};
+    
+    bool solve(vector<vector<char>>& board, string &word,int indx, int i, int j,int m , int n,vector<vector<bool>>&visited){
+        if(indx == word.length()) return true;
 
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j]) return false;
+        if(word[indx] != board[i][j]){
+            return false;
+        }
+
+        visited[i][j]= true;
         for(int k=0;k<4;k++){
-            int new_i= i+directions[k][0];
-            int new_j= j+directions[k][1];
-            if(solve(new_i, new_j, board, indx+1, word)){
+            int ni= i+dx[k];
+            int nj= j+dy[k];
+            if(solve(board, word, indx+1, ni, nj , m ,n,visited)){
                 return true;
             }
         }
-        board[i][j]= temp;
+        visited[i][j]= false;
         return false;
     }
-    
     bool exist(vector<vector<char>>& board, string word) {
-        m= board.size();
-        n= board[0].size();
+        int m = board.size();
+        int n = board[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]== word[0]  && solve(i,j, board,0, word)){
-                    return true;
+                if(board[i][j]== word[0]){
+                    vector<vector<bool>>visited(m,vector<bool>(n,false));
+                    if(solve(board, word,0, i, j, m, n, visited)){
+                        return true;
+                    }
                 }
             }
         }
