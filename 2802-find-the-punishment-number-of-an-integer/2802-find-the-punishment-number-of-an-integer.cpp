a2f8d21743249e5dce7e7fb29a83b714b4e4cdfa
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-07 00:59:30 +0530

    Time: 253 ms (34.88%), Space: 11.3 MB (20.93%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -8,7 +8,7 @@ public:
         if(indx==num.size()) return false;
 
         for(int i= indx;i<num.size();i++){
-            if(i!=indx && num[indx]=='0') break;
+            // if(i!=indx && num[indx]=='0') break;
             string part= num.substr(indx, i-indx+1);
             int val = stoi(part);
             if(ispossible(target-val, num, i+1)){
