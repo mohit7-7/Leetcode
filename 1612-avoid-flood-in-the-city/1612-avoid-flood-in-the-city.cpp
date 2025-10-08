1d2dd42e467173309e9c6f934639b141a2540959
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-09 00:48:58 +0530

    Time: 104 ms (89.74%), Space: 114.5 MB (72.93%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        const int n=rains.size();
-        vector<int> ans(n, 1); // dry lake 1 by default
-        unordered_map<int,int> rainDay; // lake -> last rain day
-        rainDay.reserve(n);
-        set<int> dry; //dry days
+        int n=rains.size();
+        vector<int> ans(n, 1);
+        unordered_map<int,int> rainDay;
+        set<int> dry;
 
         for (int i=0; i<n; i++) {
             if (rains[i]==0) 
-                dry.insert(i); //dry day 
+                dry.insert(i);  
             else {
-                const int lake=rains[i];
-                const auto it0=rainDay.find(lake);
-                if (it0!=rainDay.end()) {
-                    int prev=it0->second;
+                int lake=rains[i];
+                if (rainDay.count(lake)) {
+                    int prev=rainDay[lake];
                     // find dry day after prev
                     auto it=dry.lower_bound(prev);
                     if (it==dry.end()) return {}; // no dry day available
-                    int d=*it;
-                    ans[d]=lake; // dry this lake on day d
-                    dry.erase(it); //  dry day used
+                    int day=*it;
+                    ans[day]=lake; // dry this lake on day d
+                    dry.erase(*it); //  dry day used
                 }
                 rainDay[lake]=i;
                 ans[i]=-1; // rainy day
