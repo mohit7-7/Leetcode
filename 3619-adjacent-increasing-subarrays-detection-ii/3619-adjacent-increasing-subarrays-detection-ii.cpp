d6151861accb19addd2e143585ce5221d37f3a5f
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-10-22 03:48:22 +0530

    Time: 249 ms (14.55%), Space: 293 MB (7.51%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int maxIncreasingSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        int up = 1, preUp = 0, res = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[i - 1]) up++;
-            else {
-                preUp = up;
-                up = 1;
-            }
-            int half = up >> 1;
-            int m = min(preUp, up);
-            int candidate = max(half, m);
-            if (candidate > res) res = candidate;
+    bool check(int k, vector<int>inc){
+        int n= inc.size();
+        for(int i=k;i<n;i++){
+            if(inc[i]>=k && inc[i-k]>=k) return true;
         }
-        return res;
+        return false;
+    }
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n= nums.size();
+        vector<int>inc(n,1);
+        for(int i=1;i<n;i++){
+            if(nums[i]> nums[i-1]) inc[i]= inc[i-1]+1;
+        }
+
+        int ans=1;
+        int low =1, high= n/2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(check(mid,inc)){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high= mid-1;
+            }
+        }
+        return ans;
     }
 };
+
+// 1 2 3 4 5 6 n = 6 so max k =3
