be974ab6f5c9d45c8df11f0fee31b0190e471fa6
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-11-15 00:43:19 +0530

    Time: 39 ms (52.04%), Space: 86.3 MB (89.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
         vector<vector<int>>ans(n, vector<int>(n,0));
-        for( vector<int>&query : queries ){
-            int starti= query[0], startj= query[1], endi= query[2], endj= query[3];
-            for(int i=starti; i<=endi; i++){
-                for(int j=startj; j<=endj;j++){
-                    ans[i][j]++;
+        for( vector<int>&q : queries ){
+            int si = q[0] , sj = q[1] , endi = q[2] , endj = q[3];
+            for(int i=si;i<=endi;i++){
+                ans[i][sj]++;
+                if(endj+1 < n){
+                    ans[i][endj+1]-=1;
                 }
+            }
+        }
+
+        for(int i=0;i<n;i++){ 
+            for(int j=1;j<n;j++){
+                ans[i][j]+= ans[i][j-1];
             }
         }
         return ans;
