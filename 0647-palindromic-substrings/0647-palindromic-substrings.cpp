66ceaf208500bcbc751e1c1fa684671f9a766490
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-11-16 22:16:24 +0530

    Time: 1871 ms (5.02%), Space: 419.1 MB (8.73%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -4,11 +4,12 @@ public:
     bool check(int i, int j, string s){
         if(i>j) return true;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j]){
-            return dp[i][j]= check(i+1,j-1,s);
+        if(s[i]!= s[j]){
+            return dp[i][j]= false;
         }
-        return dp[i][j]=0;
+        return dp[i][j]= check(i+1, j-1,s);
     }
+
     int countSubstrings(string s) {
         int n= s.length();
         memset(dp,-1,sizeof(dp));
