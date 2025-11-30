904b163148f75d59d15ffa82a07bd8f48ea6cb7e
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2025-11-30 18:29:05 +0530

    Time: 137 ms (40%), Space: 173.9 MB (40%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -6,9 +6,7 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0 ; i<n ;i++){
             int j = upper_bound(nums.begin(), nums.end(), nums[i])-nums.begin();
-            int greater;
-            if(i==j) greater= n-1-j;
-            else greater = n-j;
+            int greater = n-j;
             if(greater >= k) count++;
         }
         return count;
