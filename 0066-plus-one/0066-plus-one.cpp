847583a2436910786ab340350f4d3e78c6732ecf
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2026-01-01 22:08:30 +0530

    Time: 0 ms (100%), Space: 11.7 MB (14.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        for(int i =n-1; i>=0; i--){
+        for(int i=n-1; i>=0; i--){
             digits[i]++;
             if(digits[i]!=10){
                 return digits;
             }
             digits[i]=0;
         }
-        digits.insert(digits.begin(),1);
-        return digits;
+        vector<int> temp(digits.size()+1,0);
+        temp[0]=1;
+        return temp;
     }
 };
