5cb777e4b28664e9e7b23598e81b27cbf875b6a0
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2026-01-09 01:23:01 +0530

    Time: 27 ms (58.09%), Space: 13.8 MB (95.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int mincoins(int n, int amount, vector<vector<int>>& dp, vector<int>& coins) {
-        if (n == 0) return (amount == 0) ? 0 : 1e9;
-
-        if (dp[n][amount] != -1) return dp[n][amount];
-
-        if (coins[n - 1] <= amount) {
-            int include = 1 + mincoins(n, amount - coins[n - 1], dp, coins);
-            int exclude = mincoins(n - 1, amount, dp, coins);
-            return dp[n][amount] = min(include, exclude);
-        } else {
-            return dp[n][amount] = mincoins(n - 1, amount, dp, coins);
-        }
+    int t[13][10001];
+    int solve(vector<int>&coins, int amount, int i){
+        if(amount ==0) return 0;
+        if(i<0) return 1e9;
+        if(t[i][amount]!=-1) return t[i][amount];
+        int ans1= INT_MAX;
+        if(coins[i]<= amount){
+            ans1 = 1+solve(coins, amount-coins[i], i);
+        } 
+        int ans2 = solve(coins , amount , i-1);
+        return t[i][amount]=min(ans1, ans2);
     }
-
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        int ans = mincoins(n, amount, dp, coins);
-        return (ans >= 1e9) ? -1 : ans;
+        int n= coins.size();
+        memset(t,-1,sizeof(t));
+        int ans=  solve(coins , amount, n-1);
+        if(ans < 1e9) return ans ;
+        else return -1;
     }
 };
