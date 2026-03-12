f348af6a4fe4ddfece100b528438d0480db22cfe
Mohit Mahara <143642715+mohit7-7@users.noreply.github.com>
2026-03-13 00:18:12 +0530

    Time: 311 ms (22.34%), Space: 361.4 MB (11.17%) - LeetHub

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,118 +1,139 @@
-#include <bits/stdc++.h>
-using namespace std;
+class DSU {
+public:
+    vector<int> parent, rank;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    bool Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) 
+            return false;
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+
+        return true;
+    }
+};
 
 class Solution {
 public:
 
-    struct DSU{
-        vector<int> parent;
-
-        DSU(int n){
-            parent.resize(n);
-            for(int i=0;i<n;i++) parent[i]=i;
-        }
-
-        int find(int x){
-            if(parent[x]==x) return x;
-            return parent[x]=find(parent[x]);
-        }
-
-        bool unite(int a,int b){
-            a=find(a); b=find(b);
-            if(a==b) return false;
-
-            parent[b]=a;
-            return true;
-        }
-    };
-
-    bool can(int X,int n,vector<vector<int>>& edges,int k){
-
+    bool check(int n, vector<vector<int>>& edges, int k, int mid) {
         DSU dsu(n);
-        int used=0;
-        int upgrades=0;
 
-        for(auto &e:edges){
-            int u=e[0],v=e[1],s=e[2],must=e[3];
+        vector<vector<int>> upgradeCandidates;
 
-            if(must){
-                if(s < X) return false;
-                if(!dsu.unite(u,v)) return false;
-                used++;
+        for(auto &edge : edges) { //E
+            int u = edge[0];
+            int v = edge[1];
+
+            int s = edge[2];
+            int m = edge[3];
+
+            if(m == 1) {
+                if(s < mid) {
+                    return false;
+                }
+
+                dsu.Union(u, v); //alpa
+            } else {
+                if(s >= mid) {
+                    //no need to upgrade
+                    dsu.Union(u, v);
+                } else if(2*s >= mid) {
+                    upgradeCandidates.push_back({u, v});
+                }
             }
         }
 
-        vector<pair<int,int>> cand;
+        for(auto &edge : upgradeCandidates) { //O(E)
+            int u = edge[0];
+            int v = edge[1];
 
-        for(int i=0;i<edges.size();i++){
-            auto &e=edges[i];
-            int s=e[2],must=e[3];
-
-            if(must) continue;
-            /*Add those edges whose strnegth are grater that current 
-                strenth 
-            */
-            if(s>=X) cand.push_back({0,i});
-            else if(2*s>=X) cand.push_back({1,i});
-        }
-
-        sort(cand.begin(),cand.end());
-
-        for(auto &[cost,i]:cand){
-
-            auto &e=edges[i];
-            int u=e[0],v=e[1];
-
-            if(dsu.unite(u,v)){
-                upgrades+=cost;
-                used++;
-                if(upgrades>k) return false;
-                if(used==n-1) return true;
+            if(dsu.find(u) != dsu.find(v)) {
+                if(k <= 0)
+                    return false;
+                
+                dsu.Union(u, v);
+                k--; //upgrade
             }
         }
 
-        return used==n-1;
+
+        int root = dsu.find(0);
+        for(int node = 1; node <= n-1; node++) { //O(n)
+            if(dsu.find(node) != root)
+                return false;
+        }
+        return true;
     }
 
     int maxStability(int n, vector<vector<int>>& edges, int k) {
          /*
-            first thing to notice is we must connetc all the edges that our must be 
-            connected according to the problem statement 
-
-
-            so from it all edges will have their own strength and they will not be
-            upgraded so it will have a minimum strength let's say X 
-
-
-            so now our main goal is to find some edges from the remaiing ones
-            whose strength is strength>=X to maximise the answer
-
-            or whose 2*strength >=X to maximse the answer and use K-1 operations for rest
-
-            this is optimal thing to do 
+            Input: n = 3, edges = [[0,1,1,1],[1,2,1,1],[2,0,1,1]], k = 0
+            0 --- 1 --- 2 , parent = 0
+            (2, 0)
+            2 ka. parent kaun hai = 0
+            0 ka parent kaun hai  = 0
 
         */
 
-        /*
-            Now to Connect edges one by one we can use dsu .....
+        DSU dsu(n);
+        for(auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
 
-            To maximise our minumum strength we can use binary search on answer
-            becase we have a X now we want to find strength>=X ...strength >=X+1...
+            int s = edge[2];
+            int m = edge[3];
 
-            for this binary search can be used 
-        */
-        int l=0,r=2e9,ans=-1;
-
-        while(l<=r){
-            int mid=l+(r-l)/2;
-
-            if(can(mid,n,edges,k)){
-                ans=mid;
-                l=mid+1;
+            if(m == 1) { //must be included in the spanning tree
+                if(dsu.find(u) == dsu.find(v))
+                    return -1;
+                
+                dsu.Union(u, v);
             }
-            else r=mid-1;
         }
 
-        return ans;
+
+        int result = -1;
+        int l = 1;
+        int r = 2*1e5;
+
+        //T.C : O(nlogn)
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(check(n, edges, k, mid)) {
+                result = mid;
+                l = mid+1;
+            } else {
+                r = mid -1;
+            }
+        }
+
+        return result;
     }
 };
